dc9ae38b196d6e7a099c5c5a2f3a7756c1242487
Trevor R <rogerst@my.lanecc.edu>
2019-03-15 16:01:35 -0700

    init

---
+++ CustomerMaintenanceStart/CustomerMaintenanceClasses/Customer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerMaintenanceClasses
+{
+    public class Customer
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public Customer() { }
+
+        public Customer(string first, string last, string em)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Email = em;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string GetDisplayText()
+        {
+            string info;
+            info = firstName + " " + lastName + ", " + email;
+            return info;
+        }
+    }
+}
---
+++ CustomerMaintenanceStart/CustomerTests/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerMaintenanceClasses;
+
+namespace CustomerTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCustomerConstructor();
+            TestCustomerPropertyGetter();
+            TestCustomerPropertySetter();
+            TestCustomerGetDisplayText();
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static void TestCustomerConstructor()
+        {
+            Customer customer1 = new Customer();
+            Customer customer2 = new Customer("Trevor", "Rogers", "rogerst@my.lanecc.edu");
+
+            Console.WriteLine("Testing constructors.");
+            Console.WriteLine("Default constructor. Expecting default values." + customer1.GetDisplayText());
+            Console.WriteLine("Overloaded constructor. Expecting Trevor Rogers, rogerst@my.lanecc.edu. " + customer2.GetDisplayText());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerPropertyGetter()
+        {
+            Customer customer = new Customer("Trevor", "Rogers", "rogerst@my.lanecc.edu");
+
+            Console.WriteLine("Testing getters.");
+            Console.WriteLine("Testing first name. Expecting Trevor. " + customer.FirstName);
+            Console.WriteLine("Testing last name. Expecting Rogers. " + customer.LastName);
+            Console.WriteLine("Testing email. Expecting rogerst@my.lanecc.edu. " + customer.Email);
+            Console.WriteLine();
+
+        }
+
+        static void TestCustomerPropertySetter()
+        {
+            Customer customer = new Customer("Trevor", "Rogers", "rogerst@my.lanecc.edu");
+
+            Console.WriteLine("Testing setters.");
+            customer.FirstName = "Griffin";
+            customer.LastName = "McElroy";
+            customer.Email = "test@test.com";
+
+            Console.WriteLine("Expecting Griffin McElroy, test@test.com. " + customer.GetDisplayText());
+            Console.WriteLine();
+        }
+
+        static void TestCustomerGetDisplayText()
+        {
+            Customer customer = new Customer("Trevor", "Rogers", "rogerst@my.lanecc.edu");
+
+            Console.WriteLine("Testing GetDisplayText() method.");
+            Console.WriteLine("Expecting Trevor Rogers, rogerst@my.lanecc.edu. " + customer.GetDisplayText());
+            Console.WriteLine();
+        }
+    }
+}
